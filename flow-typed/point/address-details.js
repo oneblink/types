885b5c3335dbffdfff40052f9b640b6663d5000c
Zac Turner <zac@oneblink.io>
2021-04-07 15:33:42 +1000

    Changed flow interfaces to types

--- flow-typed/point/address-details.js
+++ flow-typed/point/address-details.js
@@ -11,55 +11,55 @@ export type CadastralParcels = {
   [k: string]: mixed,
 }[]
 
-export interface PointAddressResponse {
-  data: Address;
-  requestTimestamp: string;
-  requestDate: string;
-  [k: string]: mixed;
+export type PointAddressResponse = {
+  data: Address,
+  requestTimestamp: string,
+  requestDate: string,
+  [k: string]: mixed,
 }
-export interface Address {
-  dataset?: string;
-  geo?: Geo;
-  addressDetails?: AddressDetails;
-  addressId: string;
-  addressRecordType: 'Primary' | 'Secondary';
-  asgsMain?: ASGSMain;
-  commonwealthElectorate?: CommonwealthElectorate;
-  localGovernmentArea?: LocalGovernmentArea;
-  stateElectorate?: StateElectorate;
-  cadastralParcels?: CadastralParcels;
-  [k: string]: mixed;
+export type Address = {
+  dataset?: string,
+  geo?: Geo,
+  addressDetails?: AddressDetails,
+  addressId: string,
+  addressRecordType: 'Primary' | 'Secondary',
+  asgsMain?: ASGSMain,
+  commonwealthElectorate?: CommonwealthElectorate,
+  localGovernmentArea?: LocalGovernmentArea,
+  stateElectorate?: StateElectorate,
+  cadastralParcels?: CadastralParcels,
+  [k: string]: mixed,
 }
-export interface Geo {
-  geoDatumCode?: string;
-  geoFeature?: string;
+export type Geo = {
+  geoDatumCode?: string,
+  geoFeature?: string,
   geometry?: {
     coordinates?: number[],
     type?: string,
     [k: string]: mixed,
-  };
-  [k: string]: mixed;
+  },
+  [k: string]: mixed,
 }
-export interface AddressDetails {
-  cadastralIdentifier?: string;
-  formattedAddress?: string;
-  localityName?: string;
-  postcode?: string;
-  stateTerritory?: string;
-  streetName?: string;
-  streetNumber1?: string;
-  streetNumber2?: string;
-  streetType?: string;
-  streetTypeDescription?: string;
-  lotIdentifier?: string;
-  localityNeighbours?: string;
-  gnafLocalityPid?: string;
-  gnafStreetLocalityPid?: string;
-  aliasPrincipal?: string;
-  deliveryPointIdentifier?: string;
-  [k: string]: mixed;
+export type AddressDetails = {
+  cadastralIdentifier?: string,
+  formattedAddress?: string,
+  localityName?: string,
+  postcode?: string,
+  stateTerritory?: string,
+  streetName?: string,
+  streetNumber1?: string,
+  streetNumber2?: string,
+  streetType?: string,
+  streetTypeDescription?: string,
+  lotIdentifier?: string,
+  localityNeighbours?: string,
+  gnafLocalityPid?: string,
+  gnafStreetLocalityPid?: string,
+  aliasPrincipal?: string,
+  deliveryPointIdentifier?: string,
+  [k: string]: mixed,
 }
-export interface ASGSMain {
+export type ASGSMain = {
   '2011'?: {
     mbId?: string,
     sa1Id?: string,
@@ -70,7 +70,7 @@ export interface ASGSMain {
     sa4Id?: string,
     sa4Name?: string,
     [k: string]: mixed,
-  };
+  },
   '2016'?: {
     mbId?: string,
     sa1Id?: string,
@@ -81,17 +81,17 @@ export interface ASGSMain {
     sa4Id?: string,
     sa4Name?: string,
     [k: string]: mixed,
-  };
-  [k: string]: mixed;
+  },
+  [k: string]: mixed,
 }
-export interface CommonwealthElectorate {
-  commElectoralName?: string;
-  commElectoralPid?: string;
-  [k: string]: mixed;
+export type CommonwealthElectorate = {
+  commElectoralName?: string,
+  commElectoralPid?: string,
+  [k: string]: mixed,
 }
-export interface LocalGovernmentArea {
-  lgaName?: string;
-  lgaPid?: string;
-  lgaShortName?: string;
-  [k: string]: mixed;
+export type LocalGovernmentArea = {
+  lgaName?: string,
+  lgaPid?: string,
+  lgaShortName?: string,
+  [k: string]: mixed,
 }
