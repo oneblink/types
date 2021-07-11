a639b1425ba3cda584ecc5fa1e5f4098e34c77d5
Matt Carroll <matt.carroll139@gmail.com>
2021-07-11 20:37:04 +1000

    Added scheduling types

---
+++ flow-typed/scheduling.js
@@ -0,0 +1,22 @@
+declare type NewSchedulingBooking = {
+  submissionId: string,
+  formId: number,
+  schedulingReceiptUrl: string,
+  schedulingCancelUrl: string,
+  nylasSchedulingPageId: number,
+}
+
+declare type SchedulingBooking = NewSchedulingBooking & {
+  nylasCalendarId?: string,
+  nylasEventId?: string,
+  nylasEditHash?: string,
+  emailAddress?: string,
+  name?: string,
+  location?: string,
+  startTime?: number,
+  endTime?: number,
+  timezone?: string,
+  cancelledReason?: string,
+  createdAt?: string,
+  updatedAt?: string,
+}
