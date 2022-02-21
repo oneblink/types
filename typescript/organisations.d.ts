2de4a8f7e3d073184418411e42652ba656327017
Matt Carroll <matt.carroll139@gmail.com>
2022-02-21 11:28:49 +1100

    Added FormsAppSendingAddressResend type to auditing

--- typescript/organisations.d.ts
+++ typescript/organisations.d.ts
@@ -112,6 +112,7 @@ export type AuditRecordType =
   | 'FormsAppDraft'
   | 'FormsAppUserSubscription'
   | 'FormsAppSendingAddress'
+  | 'FormsAppSendingAddressResend'
   | 'Integration'
   | 'FormApprovalFlowInstance'
   | 'FormSubmissionApproval'
