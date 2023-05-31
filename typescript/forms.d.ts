ed6b67f92668b65cecd38376a01545fb8ae0c917
kizaonline <kizaonline@users.noreply.github.com>
2023-05-31 16:59:17 +1000

    Update typescript/forms.d.ts
    Co-authored-by: Matt Carroll <matt.carroll139@gmail.com>

--- typescript/forms.d.ts
+++ typescript/forms.d.ts
@@ -1168,10 +1168,10 @@ export type FormElementLookupTableColumnNumber = {
 export type FormElementLookupTableEnvironment = {
   formsAppEnvironmentId: number
   /** array of elements with prefil values for each corresponding input value */
-  columns: (
+  columns: Array(
     | FormElementLookupTableColumnText
     | FormElementLookupTableColumnNumber
-  )[]
+  >
 }
 
 export type NewFormElementLookupBase = {
