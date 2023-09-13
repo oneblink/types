48e1bc239e4cfc03b05d4a5fa68a68ef37e6ea97
josh215 <josh@oneblink.io>
2023-09-13 12:15:53 +1000

    ON-34592 # Removed double slash

--- typescript/scheduledTasks.ts
+++ typescript/scheduledTasks.ts
@@ -1,39 +1,39 @@
 export type DayOfWeek = 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN'
 
 type BaseTaskAction = {
-  //** The label of the action */
+  /** The label of the action */
   label: string
-  //** The direction a user will have to swipe for this action to occur */
+  /** The direction a user will have to swipe for this action to occur */
   swipeDirection: 'LEFT' | 'RIGHT'
 }
 export type TaskAction = BaseTaskAction &
   (
     | {
         type: 'FORM'
-        //** The related form id that will be used for the action */
+        /** The related form id that will be used for the action */
         formId: number
       }
     | {
         type: 'CHANGE_STATUS'
-        //** The status that will be set on the task for the action */
+        /** The status that will be set on the task for the action */
         status: 'COMPLETE'
       }
   )
 
 export interface NewTask {
-  //** The name of the task */
+  /** The name of the task */
   name: string
-  //** The related forms app environment id that this task belongs to */
+  /** The related forms app environment id that this task belongs to */
   formsAppEnvironmentId: number
   /** The organisation id that this task belongs to */
   organisationId: string
-  //** The schedule that will determine a task's frequency and date range */
+  /** The schedule that will determine a task's frequency and date range */
   schedule: {
-    //** The date a task becomes available */
+    /** The date a task becomes available */
     startDate?: string
-    //** The date a task becomes unavailable */
+    /** The date a task becomes unavailable */
     endDate?: string
-    //** The sequence in which the task will occur */
+    /** The sequence in which the task will occur */
     recurrence:
       | {
           interval: 'DAY'
@@ -49,10 +49,10 @@ export interface NewTask {
   actions: TaskAction[]
 }
 export type Task = NewTask & {
-  //** The id of the related Task */
+  /** The id of the related Task */
   id: number
-  //** The time the task was created */
+  /** The time the task was created */
   createdAt: string
-  //** The time the task was last updated  */
+  /** The time the task was last updated */
   updatedAt: string
 }
