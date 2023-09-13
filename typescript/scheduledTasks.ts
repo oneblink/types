64a49f998228b3f3bf7903031950d5e714e947c1
Zac Turner <zac@oneblink.io>
2023-09-13 12:20:40 +1000

    Changed `swipeDirection` to `icon`

--- typescript/scheduledTasks.ts
+++ typescript/scheduledTasks.ts
@@ -3,7 +3,7 @@ import { MiscTypes } from '..'
 export type DayOfWeek = 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN'
 type BaseTaskAction = {
   label: string
-  swipeDirection: 'LEFT' | 'RIGHT'
+  icon: string
 }
 export type TaskAction = BaseTaskAction &
   (
@@ -43,19 +43,20 @@ export type Task = NewTask & {
 }
 
 export interface CompletedTask {
-  /** the id of the app displaying the tasks */
+  /** The id of the app displaying the tasks */
   formsAppId: number
-  /** the id of the task */
+  /** The id of the task */
   taskId: number
-  /** the submissionId relating to the form action  */
+  /** The submissionId relating to the form action */
   submissionId?: string
-  /** the user which actioned the task */
+  /** The user which actioned the task */
   completedBy: MiscTypes.UserProfile
-  /** the timestamp for when the user marked the task as done */
+  /** The timestamp for when the user marked the task as done */
   createdAt: string
-  /** if there is no form action then this will be the same as createdAt
-   * if there is a form action this will be the timestamp for when a lambda
-   * has triggered after the uploading of the s3 data
+  /**
+   * If there is no form action then this will be the same as createdAt if there
+   * is a form action this will be the timestamp for when a lambda has triggered
+   * after the uploading of the s3 data
    */
   completedAt?: string
 }
