75dd9f07eace445232aa82ff77340539deda3730
Zac Turner <zac@oneblink.io>
2023-11-21 11:37:15 +1100

    Updated forms app TGI definition type

--- typescript/formsApps.d.ts
+++ typescript/formsApps.d.ts
@@ -52,7 +52,7 @@ export type FormsAppScheduledTasksGroupMenuItem = FormsAppBaseMenuItem & {
   /** Type of menu item */
   type: 'SCHEDULED_TASK_GROUPS'
   /** The ids of the task groups assigned to the app */
-  taskGroupInstances: Array<TaskGroupInstance>
+  taskGroupInstances: Array<{ taskGroupInstanceId: TaskGroupInstance['id'] }>
   /** If true, menu item will be the default item shown */
   isDefault: boolean
 }
