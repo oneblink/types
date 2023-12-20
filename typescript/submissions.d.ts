745c5b053545a6419fab923f43020920b255d984
Matt Carroll <matt.carroll139@gmail.com>
2023-12-20 15:57:13 +1100

    Added WestpacQuickStreamPayment type

--- typescript/submissions.d.ts
+++ typescript/submissions.d.ts
@@ -8,6 +8,7 @@ import {
   FormWorkflowEvent,
   FormSchedulingEvent,
   NSWGovPaySubmissionEvent,
+  WestpacQuickStreamSubmissionEvent,
 } from './submissionEvents'
 import { components as cpPayV1Components } from './cp-pay/swagger.v1'
 import { components as cpPayV2Components } from './cp-pay/swagger.v2'
@@ -292,6 +293,362 @@ export type CPPayPayment = BaseFormSubmissionPayment & {
       >)
 }
 
+export type WestpacQuickStreamPayment = BaseFormSubmissionPayment & {
+  type: WestpacQuickStreamSubmissionEvent['type']
+  paymentTransaction?: {
+    /** Receipt number to display to customers. */
+    receiptNumber: string
+    /** PAYMENT, REFUND, PREAUTH, CAPTURE or ACCOUNT_VERIFICATION. */
+    transactionType: string
+    /** INCREMENTAL, EXTENSION, REAUTHORISATION. For pre-authorisation transactions only. */
+    authorisationType?: string
+    /** FULL or PARTIAL. For pre-authorisation cancellations only. */
+    cancellationType?: string
+    /**
+     * Approved, Approved*, Pending, Declined, Voided or Suspended. See
+     * Transaction status.
+     */
+    status: string
+    /** Reason code for the status. See Response Codes for more. */
+    responseCode: string
+    /** Reason description for the status. See Response Codes for more. */
+    responseDescription: string
+    /** Success indicator for the responseCode. See Response Codes for more. */
+    summaryCode: string
+    /**
+     * The result of Fraud Guard processing. null when transaction is not
+     * processed using Fraud Guard.
+     */
+    fraudGuardResult: string | null
+    /**
+     * If a refund, the original payment. If a payment, the original PREAUTH or
+     * the automatically-retried payment.
+     */
+    originalReceiptNumber?: string
+    /**
+     * Unique identifier for a customer. When this value matches a customer
+     * number, the transaction shows in their payment history.
+     */
+    customerReferenceNumber: string
+    /** Your reference for this transaction. */
+    paymentReferenceNumber: string
+    /** A comment for the transaction. */
+    comment: string
+    /** Extra information about the transaction. See Take Payment. */
+    metadata: Record<string, string>
+    /** A code indicating the source of the transaction. For example, RESTAPI. */
+    source: string
+    /** The IP address of the customer. For example, 192.168.42.184. */
+    ipAddress: string
+    /**
+     * The day on which this transaction was considered to have been processed.
+     * See Transaction Settlement for more.
+     */
+    settlementDate: string
+    /** Date and time (if known) when transaction processing was initiated. */
+    transactionTime: string
+    /** If true, this transaction may be voided. See Void payment. */
+    voidable: boolean
+    /** If true, this transaction may be refunded. See Refund payment. */
+    refundable: boolean
+    /** Amount before any surcharge added. */
+    principalAmount: {
+      /** ISO 4217 currency code. */
+      currency: string
+      /** Decimal Monetary amount to two decimal places. */
+      amount: number
+      /** A customer friendly amount you can display to your customers */
+      displayAmount: string
+    }
+    /** Amount of surcharge. See surcharges. */
+    surchargeAmount: {
+      /** ISO 4217 currency code. */
+      currency: string
+      /** Decimal Monetary amount to two decimal places. */
+      amount: number
+      /** A customer friendly amount you can display to your customers */
+      displayAmount: string
+    }
+    /** Total amount of transaction. Principal amount plus surcharge. */
+    totalAmount: {
+      /** ISO 4217 currency code. */
+      currency: string
+      /** Decimal Monetary amount to two decimal places. */
+      amount: number
+      /** A customer friendly amount you can display to your customers */
+      displayAmount: string
+    }
+    /** For credit card payments, your merchant facility. */
+    merchantAccount?: {
+      /** Uniquely identifies a merchant facility. Issued by us. */
+      merchantId: string
+      merchantName: string
+      /**
+       * The BSB of your settlement bank account. Australian Westpac merchant
+       * facilities only.
+       */
+      settlementBsb: string
+      /**
+       * The account number of your settlement bank account. Australian Westpac
+       * merchant facilities only.
+       */
+      settlementAccountNumber: string
+      /**
+       * The BSB of your surcharge settlement bank account. Only present if a
+       * separate surcharge account is utilised. Australian Westpac merchant
+       * facilities only.
+       */
+      settlementSurchargeBsb?: string
+      /**
+       * The account number of your surcharge settlement bank account. Only
+       * present if a separate surcharge account is utilised. Australian Westpac
+       * merchant facilities only.
+       */
+      settlementSurchargeAccountNumber?: string
+      /** A customer-friendly display name. */
+      displayName: string
+      /**
+       * A code describing the aquiring financial institution for this merchant
+       * facility. Usually WBC.
+       */
+      acquiringInstitution: string
+      /** The currency accepted by this merchant facility. */
+      currency: string
+    }
+    /** For Australian bank account payments, your direct entry settlement account. */
+    directEntryAccount?: {
+      /** A customer-friendly display name. */
+      displayName: string
+      /** The currency of the account. For example, AUD. */
+      currency: string
+      /** A unique direct entry user identification number, issued by us. */
+      directEntryUserId: string
+      /**
+       * The name associated with the direct entry user Id. Australian Westpac
+       * accounts only.
+       */
+      directEntryUserName?: string
+      /** Name used to open your bank account. */
+      accountName: string
+      /** The BSB of your settlement bank account. */
+      settlementBsb: string
+      /** The account number of your settlement bank account. */
+      settlementAccountNumber: string
+      /** Trading Name of your business. This appears on your customer's bank statement. */
+      remitterName: string
+    }
+    /** For New Zealand bank account payments, your direct entry settlement account. */
+    nzDirectEntryAccount?: {
+      /** A customer-friendly display name. */
+      displayName: string
+      /** The currency of the account. For example, NZD. */
+      currency: string
+      /** A unique direct entry user identification number, issued by us. */
+      debitUserId: string
+      /** Name used to open your bank account. */
+      nzAccountName: string
+      /** The bank code of your settlement bank account. */
+      nzBankCode: string
+      /** The branch code of your settlement bank account. */
+      nzBranchCode: string
+      /** The account number of your settlement bank account. */
+      nzAccountNumber: string
+      /** The account number suffix of your settlement bank account. */
+      nzAccountSuffix: string
+    }
+    /** For credit card payments, your customer's credit card. */
+    creditCard?: {
+      /** CREDIT_CARD */
+      accountType: string
+      /** The account token. See Register Account for more. */
+      accountToken: string
+      /**
+       * If true, this account will be used for the owning customer where an
+       * account token is not specified. This includes taking payments by
+       * customerId and creating recurring payments without an accountToken.
+       */
+      defaultAccount: boolean
+      /**
+       * Masked credit card number displaying the first 6 and last 3 digits. For
+       * scheme tokenised accounts, this would display the last 3 digits in the
+       * following format: xxxxxx...242.
+       */
+      cardNumber: string
+      /** Two digit expiry month. */
+      expiryDateMonth: string
+      /** Two digit expiry year. */
+      expiryDateYear: string
+      /** The card scheme. VISA, MASTERCARD, AMEX, DINERS, JCB, or UNIONPAY. */
+      cardScheme: string
+      /**
+       * The card type. CREDIT, DEBIT. Note: This is only for VISA and
+       * MASTERCARD. Other card types may be added in the future.
+       */
+      cardType: string
+      /** The name printed on the card. */
+      cardholderName: string
+      /**
+       * Masked credit card number displaying the first 6 and last 4 digits. For
+       * scheme tokenised accounts, this would display the last 4 digits in the
+       * following format: xxxxxx...4242.
+       */
+      maskedCardNumber4Digits: string
+      /**
+       * FPAN (default) - the card number from a physical card. Also known as
+       * "Funding PAN". DPAN - a digitised card number from a wallet provider or
+       * scheme tokenisation service. Also known as "Digital PAN".
+       */
+      panType: string
+      /**
+       * Specifies the wallet provider from which the card details were
+       * obtained, if applicable. APPLE_PAY, GOOGLE_PAY
+       */
+      walletProvider: string
+      /**
+       * QuickStream's unique identifier for the customer. This customer is
+       * created automatically when using Register account without customer
+       * details or by instruction from your server when using Create customer
+       * and then Register customer account. See Get Customer for more.
+       */
+      customerId: string
+      /** Links to related documents and resources. */
+      links: Array<{
+        /**
+         * The relationship to the resource. Open help URLs in a browser to view
+         * this developer reference. next and prev are used for paginated resources.
+         */
+        rel: string
+        /** URL of a related document or resource. */
+        href: string
+        /** HTTP Verb */
+        requestMethod: string
+      }>
+    }
+    /** For Australian bank account payments, your customer's bank account. */
+    bankAccount?: {
+      /** Name of account holder. */
+      accountName?: string
+      /** The bank-state-branch holding their account. */
+      bsb: string
+      /** The account number at that branch. */
+      accountNumber: string
+    }
+    /** For New Zealand bank account payments, your customer's bank account. */
+    nzBankAccount?: {
+      /** DIRECT_DEBIT_NZ */
+      accountType: string
+      /** The account token. See Register Account for more. */
+      accountToken: string
+      /**
+       * If true, this account will be used for the owning customer where an
+       * account token is not specified. This includes taking payments by
+       * customerId and creating recurring payments without an accountToken.
+       */
+      defaultAccount: boolean
+      /** Name of account holder. */
+      nzAccountName: string
+      /** The bank account display name. */
+      displayName: string
+      /** NZD */
+      currency: string
+      /** The bank holding their account. */
+      nzBankCode: string
+      /** The branch holding their account. */
+      nzBranchCode: string
+      /** The account number at that branch. */
+      nzAccountNumber: string
+      /** The type of account. */
+      nzAccountSuffix: string
+      /**
+       * QuickStream's unique identifier for the customer. This customer is
+       * created automatically when using Register account without customer
+       * details or by instruction from your server when using Create customer
+       * and then Register customer account. See Get Customer for more.
+       */
+      customerId: string
+      /** Links to related documents and resources. */
+      links: Array<{
+        /**
+         * The relationship to the resource. Open help URLs in a browser to view
+         * this developer reference. next and prev are used for paginated resources.
+         */
+        rel: string
+        /** URL of a related document or resource. */
+        href: string
+        /** HTTP Verb */
+        requestMethod: string
+      }>
+    }
+    /**
+     * This field is only relevant for credit card payments for Aggregators. The
+     * sub-merchant name.
+     */
+    merchantName?: string
+    /**
+     * This field is only relevant for credit card payments for Aggregators. The
+     * street address of the sub-merchant's trading address.
+     */
+    merchantStreetAddress?: string
+    /**
+     * This field is only relevant for credit card payments for Aggregators. The
+     * city or suburb of the sub-merchant's trading address.
+     */
+    merchantLocation?: string
+    /**
+     * This field is only relevant for credit card payments for Aggregators.The
+     * state of the sub-merchant's trading address.
+     */
+    merchantState?: string
+    /** This field is only relevant for credit card payments for Aggregators. Always AU. */
+    merchantCountry?: string
+    /**
+     * This field is only relevant for credit card payments for Aggregators. The
+     * postal code of the sub-merchant's trading address.
+     */
+    merchantPostCode?: string
+    /**
+     * This field is only relevant for credit card payments for Aggregators. A
+     * unique identifier for your sub-merchant. The aggregator allocates the
+     * sub-merchant identifier. Westpac does not allocate this value.
+     */
+    subMerchantId?: string
+    /**
+     * The authorisation code returned from the issuing bank for pre-auth
+     * transactions (at most 6 characters). Only for approved pre-auth transactions.
+     */
+    authorisationCode?: string
+    /** @deprecated Use networkTransactionId. */
+    authorisationTraceId: string
+    /**
+     * Returned by Visa and Mastercard to identify the lifecycle of a pre-auth
+     * or purchase (at most 15 characters). In most cases you can ignore this field.
+     */
+    networkTransactionId?: string
+    /** Links to related resources/documentation. */
+    links: Array<{
+      /**
+       * The relationship to the resource. Open help URLs in a browser to view
+       * this developer reference. next and prev are used for paginated resources.
+       */
+      rel: string
+      /** URL of a related document or resource. */
+      href: string
+      /** HTTP Verb */
+      requestMethod: string
+    }>
+    /**
+     * Whether this transaction was used to repay a debt. In most cases you can
+     * ignore this field.
+     */
+    debtRepayment: boolean
+    /**
+     * Additional information from the scheme about why a transaction was
+     * approved or declined.
+     */
+    merchantAdviceCode: string
+  }
+}
+
 export type WestpacQuickWebPayment = BaseFormSubmissionPayment & {
   type: WestpacQuickWebSubmissionEvent['type']
   paymentTransaction?: {
@@ -477,6 +834,7 @@ export type NSWGovPayPayment = BaseFormSubmissionPayment & {
 export type NewFormSubmissionPayment =
   | CPPayPayment
   | WestpacQuickWebPayment
+  | WestpacQuickStreamPayment
   | BPOINTPayment
   | NSWGovPayPayment
 
