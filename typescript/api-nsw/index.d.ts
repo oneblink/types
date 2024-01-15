308e87bf0c1408c52cd5e4b5277f47c5107c4855
Zac Turner <zac@oneblink.io>
2024-01-16 08:51:42 +1100

    ON-37353 # Finishing touches

--- typescript/api-nsw/index.d.ts
+++ typescript/api-nsw/index.d.ts
@@ -2,3 +2,5 @@ import { definitions } from './liquor'
 
 export type LiquorLicenseDetails = definitions['details_results']
 export type LiquorLicenseBrowseResults = definitions['browse_results']
+export type LiquorLicenceAccessTokenResponse =
+  definitions['access_token_response']
