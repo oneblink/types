f289cf71ab0d707bb2192fb1b2907798eafb1e90
David Porter <david@oneblink.io>
2024-04-29 12:19:50 +1000

    ON-39915 # add CPHCMSTokenResponse type

---
+++ typescript/cp-hcms/index.d.ts
@@ -0,0 +1,11 @@
+export type CPHCSMAccessTokenResponse = {
+  token: {
+    token_type: string
+    access_token: string
+    expires_in: number
+    scope: string
+    expires_at: number
+  }
+  appName: string
+  baseUrl: string
+}
