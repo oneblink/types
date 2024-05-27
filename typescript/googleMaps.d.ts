e26cad8efb976ac279b759af3b4790a7414083eb
Matt Carroll <matt.carroll139@gmail.com>
2024-05-27 10:15:50 +1000

    ON-40697 # Changed google address type to literals

--- typescript/googleMaps.d.ts
+++ typescript/googleMaps.d.ts
@@ -1,4 +1,9 @@
-export type GoogleMapsAddress = Pick<
-  google.maps.places.PlaceResult,
-  'place_id' | 'formatted_address' | 'geometry' | 'address_components'
->
+export type GoogleMapsAddress = {
+  place_id: google.maps.places.PlaceResult['place_id']
+  formatted_address: google.maps.places.PlaceResult['formatted_address']
+  address_components: google.maps.places.PlaceResult['address_components']
+  geometry: {
+    location?: google.maps.LatLngLiteral
+    viewport?: google.maps.LatLngBoundsLiteral
+  }
+}
