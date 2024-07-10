2bdb580606d4f76e180b74d6f8a7d3ebbffd90ba
jdawg093 <49189518+jdawg093@users.noreply.github.com>
2024-07-10 12:51:02 +1000

    ON-41889 # Added new developer key and added legacy key

--- typescript/keys.d.ts
+++ typescript/keys.d.ts
@@ -1,24 +1,36 @@
 export interface BaseKey {
+  links: {
+    organisations: string
+  }
   name: string
+}
+
+export interface NewLegacyKey extends BaseKey {
+  type?: 'LEGACY'
   privilege: {
     API_HOSTING?: 'DEPLOYMENT'
     PDF?: 'BUILDER'
     WEB_APP_HOSTING?: 'DEPLOYMENT'
     FORMS?: 'FaaS'
   }
-  links: {
-    organisations: string
+}
+
+export interface NewDeveloperKey extends BaseKey {
+  type: 'ROLE'
+  privilege: {
+    roleId: number
+    formsAppIds?: number[]
   }
 }
 
-export interface NewKey extends BaseKey {
+export type NewKey = {
   secret: string
-}
+} & (NewLegacyKey | NewDeveloperKey)
 
-export interface Key extends BaseKey {
+export type Key = {
   customerSecretId: number
   id: string
-}
+} & (NewLegacyKey | NewDeveloperKey)
 
 export type DeveloperKeyAccess = {
   submissions?: {
