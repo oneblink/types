f90d832d6bf952d4869e8985689f2e04acf12d71
kizaonline <kizaonline@gmail.com>
2024-10-01 13:43:28 +1000

    ON-43918 # Update types for conditional submission

--- typescript/forms.d.ts
+++ typescript/forms.d.ts
@@ -1084,8 +1084,13 @@ export type NewForm = {
   allowGeoscapeAddresses?: boolean
   /** Unique domain safe text to identify the form */
   slug?: string
-  /** Disables form submission based on evaluation of predicates */
-  conditionallyDisableSubmissionPredicates?: ConditionalPredicate[]
+  /** Conditionally enable form submission  */
+  enableSubmission?: {
+    /** Indicates if all predicates need to met to determine if submission should be allowed */
+    requiresAllConditionalPredicates: boolean
+    /** The predicates to determine if submission should be allowed */
+    conditionallyPredicates?: ConditionalPredicate[]
+  }
 }
 
 export type Form = MiscTypes.IdResource & NewForm
@@ -1241,44 +1246,40 @@ export type FormElementLookupStaticDataRecordBase = {
   preFills: FormElementLookupStaticDataPreFill[]
 }
 
-export type FormElementLookupStaticDataRecordText =
-  FormElementLookupStaticDataRecordBase & {
-    inputType: 'TEXT'
-    /**
-     * The value that will be matched exactly on the form element this lookup is
-     * associated when the user is completing the form.
-     */
-    inputValue: string
-  }
-export type FormElementLookupStaticDataRecordNumber =
-  FormElementLookupStaticDataRecordBase & {
-    inputType: 'NUMBER'
-    /**
-     * The value that will be matched exactly on the form element this lookup is
-     * associated when the user is completing the form.
-     */
-    inputValue: number
-  }
+export type FormElementLookupStaticDataRecordText = FormElementLookupStaticDataRecordBase & {
+  inputType: 'TEXT'
+  /**
+   * The value that will be matched exactly on the form element this lookup is
+   * associated when the user is completing the form.
+   */
+  inputValue: string
+}
+export type FormElementLookupStaticDataRecordNumber = FormElementLookupStaticDataRecordBase & {
+  inputType: 'NUMBER'
+  /**
+   * The value that will be matched exactly on the form element this lookup is
+   * associated when the user is completing the form.
+   */
+  inputValue: number
+}
 
-export type FormElementLookupStaticDataRecordUndefined =
-  FormElementLookupStaticDataRecordBase & {
-    inputType: 'UNDEFINED'
-  }
+export type FormElementLookupStaticDataRecordUndefined = FormElementLookupStaticDataRecordBase & {
+  inputType: 'UNDEFINED'
+}
 
 export type FormElementLookupStaticDataRecord =
   | FormElementLookupStaticDataRecordText
   | FormElementLookupStaticDataRecordNumber
   | FormElementLookupStaticDataRecordUndefined
 
-export type FormElementLookupStaticDataEnvironment =
-  FormElementEnvironmentBase & {
-    /**
-     * Array of records, each associated with a "inputValue" that will determine
-     * the prefill data for the configured form elements based on the
-     * "FormElement.name" property.
-     */
-    records: FormElementLookupStaticDataRecord[]
-  }
+export type FormElementLookupStaticDataEnvironment = FormElementEnvironmentBase & {
+  /**
+   * Array of records, each associated with a "inputValue" that will determine
+   * the prefill data for the configured form elements based on the
+   * "FormElement.name" property.
+   */
+  records: FormElementLookupStaticDataRecord[]
+}
 
 export type NewFormElementLookupBase = {
   /** A human readable identifier for the Lookup. */
@@ -1314,8 +1315,7 @@ export type FormElementLookup =
   | FormElementLookupUrl
   | FormElementLookupStaticData
 
-export type FormElementLookupSearchParameters =
-  FormElementOptionSetSearchParameters
+export type FormElementLookupSearchParameters = FormElementOptionSetSearchParameters
 
 export type FormElementLookupSearchResponse = {
   formElementLookups: FormElementLookup[]
