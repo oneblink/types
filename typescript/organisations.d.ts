9198a1a5b6bddb37812630e322fbaa494c8b255e
Ryan Button <ryan@oneblink.io>
2024-11-14 13:27:58 +1100

    ON-44825 # Audit type `ApprovalFormsAppMfaRequirement`

--- typescript/organisations.d.ts
+++ typescript/organisations.d.ts
@@ -177,6 +177,7 @@ export type AuditRecordType =
   | 'PDFConversion'
   | 'OrganisationManagedSecret'
   | 'FormKeyAssociation'
+  | 'ApprovalFormsAppMfaRequirement'
 
 export type NewAuditRecord = {
   type: AuditRecordType
