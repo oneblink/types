5d715f31804a396350831bd235a826ac08f1102a
David Porter <david@oneblink.io>
2025-03-24 14:30:20 +1100

    OB-2372 # prettier write

--- typescript/point/addresses.d.ts
+++ typescript/point/addresses.d.ts
@@ -6,8 +6,8 @@
  */
 
 export type Addresses = {
-  id?: string;
-  address?: string;
-  rank?: number;
-  [k: string]: unknown;
-}[];
+  id?: string
+  address?: string
+  rank?: number
+  [k: string]: unknown
+}[]
