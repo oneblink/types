1dee8c5c96cc860d81543a1f1a1657274bdd250a
Zac Turner <zac@oneblink.io>
2025-04-03 13:51:50 +1100

    ap-2486 # Added search results type

--- typescript/point/index.d.ts
+++ typescript/point/index.d.ts
@@ -1,10 +1,10 @@
 import { StatesAndTerritories } from '../misc'
 import { Address } from './address-details-v2'
-import { Addresses } from './addresses'
+import { Addresses,AddressesV3 } from './addresses'
 import { AddressResponseV3 } from './address-details-v3'
 
 export { Address as PointAddress }
 export { AddressResponseV3 as PointAddressResponseV3 }
 export { Addresses as PointAddressesSearchResult }
-
+export { AddressesV3 as PointAddressesSearchResultV3 }
 export type PointStatesAndTerritories = StatesAndTerritories | 'OT'
