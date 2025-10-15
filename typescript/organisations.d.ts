a74c2ca814971fb9217fe81ee7f9005be66a933e
Zac Turner <zac@oneblink.io>
2025-10-16 10:00:19 +1100

    ap-5543 # Added integration restrictions to pricing tiers

--- typescript/organisations.d.ts
+++ typescript/organisations.d.ts
@@ -2,6 +2,7 @@ import { FormEventType } from './submissionEvents'
 import { FormPostSubmissionAction } from './forms'
 import { UserProfile } from './misc'
 import { DeveloperKeyReference } from './keys'
+import { IntegrationType } from './integrations'
 
 export interface NewFormRetentionPolicy {
   formId: number
@@ -86,6 +87,7 @@ export interface NewTier {
     maximumFormCustomPDFs: TierLimitation
     availableFormSubmissionEvents?: FormEventType[] | null
     availableFormPostSubmissionActions?: FormPostSubmissionAction[] | null
+    availableIntegrations?: IntegrationType[] | null
     allowFormsAppPWASettings: boolean
     allowFormsAppCustomDomains: boolean
     allowFormsAppMenu: boolean
