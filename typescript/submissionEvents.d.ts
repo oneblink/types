0daa5a9e2894217f593ba50d98b0bd7d703a11ea
divporter <42131292+divporter@users.noreply.github.com>
2026-02-11 14:53:21 +1100

    Apply suggestions from code review
    Co-authored-by: Zac Turner <39893564+Zaxist@users.noreply.github.com>

--- typescript/submissionEvents.d.ts
+++ typescript/submissionEvents.d.ts
@@ -608,7 +608,7 @@ export type SalesforceCreateObjectSubmissionEvent = FormEventBase & {
   type: 'SALESFORCE_CREATE_OBJECT_RECORD'
   configuration: {
     /** The id of the OneBlink -> Salesforce integration Environment to be used. */
-    integrationKeyId: string
+    integrationEnvironmentId: string
     object: {
       id: string
       displayName: string
