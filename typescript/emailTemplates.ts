8ab4a42ffe0baaaefcb5f656ab07934703cfdb9f
Zac Turner <zac@oneblink.io>
2026-02-19 15:34:25 +1100

    ap-7222 # Corrected email templates mistake

--- typescript/emailTemplates.ts
+++ typescript/emailTemplates.ts
@@ -1,22 +1,27 @@
-import { WithCommonAssociations, WithEnvironmentAssociation } from './misc'
+import {
+  WithEnvironmentAssociation,
+  WithOrganisationAssociation,
+  WithWorkspaceAssociation,
+} from './misc'
 
 export type EmailTemplateEnvironment = WithEnvironmentAssociation & {
   template: string
 }
 
-export type NewEmailTemplate = WithCommonAssociations & {
-  name: string
+export type NewEmailTemplate = WithOrganisationAssociation &
+  WithWorkspaceAssociation & {
+    name: string
 
-  environments: EmailTemplateEnvironment[]
-  type:
-    | 'FORM_SUBMISSION_EVENT_PDF'
-    | 'FORMS_APP_WELCOME_EMAIL'
-    | 'APPROVAL_CLARIFICATION_REQUEST'
-    | 'APPROVAL_APPROVED'
-    | 'APPROVAL_DENIED'
-    | 'TO_APPROVER_APPROVAL_CREATED'
-    | 'FORM_SUBMISSION_EVENT_EMAIL'
-}
+    environments: EmailTemplateEnvironment[]
+    type:
+      | 'FORM_SUBMISSION_EVENT_PDF'
+      | 'FORMS_APP_WELCOME_EMAIL'
+      | 'APPROVAL_CLARIFICATION_REQUEST'
+      | 'APPROVAL_APPROVED'
+      | 'APPROVAL_DENIED'
+      | 'TO_APPROVER_APPROVAL_CREATED'
+      | 'FORM_SUBMISSION_EVENT_EMAIL'
+  }
 
 export type EmailTemplate = NewEmailTemplate & {
   id: number
