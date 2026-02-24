daa947eaa37fb9b4fc51a04b9b58e91d7bf763bc
Zac Turner <zac@oneblink.io>
2026-02-24 16:57:25 +1100

    ap-7222 # Added colour to workspaces

--- typescript/workspaces.ts
+++ typescript/workspaces.ts
@@ -4,6 +4,8 @@ export interface NewWorkspace extends WithOrganisationAssociation {
   label: string
   /** The icon to display on the workspace */
   icon?: string
+  /** The colour of the workspace, in hexadecimal format (for example, #0099EE) */
+  colour?: string
   /** Whether the workspace is the default workspace. An organisation can only have one default workspace. */
   isDefault?: boolean
   /** The description of the workspace */
