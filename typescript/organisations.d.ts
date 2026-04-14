a5e30e8b956945bbe11c6a7976e31e33952e66f9
Zac Turner <zac@oneblink.io>
2026-04-14 15:58:48 +1000

    ap-8003 # Changed type names of AI Builder

--- typescript/organisations.d.ts
+++ typescript/organisations.d.ts
@@ -240,6 +240,6 @@ export type ProductNotification = NewProductNotification & {
 
 export type FeatureFlag = {
   organisationId: string
-  type: 'AI_FORMS_BUILDER' | 'AI_APP_STYLIST'
+  type: 'AI_FORMS_BUILDER' | 'AI_ENVIRONMENT_APP_STYLES_BUILDER'
   allowedFormsAppEnvironmentIds: number[]
 }
